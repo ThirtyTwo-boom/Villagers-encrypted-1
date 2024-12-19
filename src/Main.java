e219aea1a6fd86d960594e686e199d37f4c6073b
ThirtyTwo-boom <621852549@qq.com>
2024-12-19 21:41:37 +0800

    Add encryption

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("请输入key");
+        int key = in.nextInt();
+        System.out.println("请输入密文");
+        in.nextLine();
+        char[] ciphertext = in.nextLine().toCharArray();
+        char[] output = new char[ciphertext.length];
+        for (int i = 0;i < ciphertext.length;i++){
+            output[i] = (char) (ciphertext[i]>>5*key<<key/2+10054>>>5);
+        }
+        System.out.println(output);
     }
 }
